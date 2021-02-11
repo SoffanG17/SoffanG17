4d606156830345e7a2edf966caa1c833e6735201
Henke <kultala@kth.se>
2021-02-11 15:02:35 +0100

    Issue-#19-Refac-Updates the signature of P2 and updates the output produced

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -40,13 +40,16 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     /*the CI server supports executing the automated tests of the group project.
     Testing is triggered as webhook, on the branch where the change has been made,
     as specified in the HTTP payload.*/
-    public void executeTests(String repoBaseDirPath) throws IOException {
+    public String executeTests(String repoBaseDirPath) {
         final String TEST_REPORT_FOLDER = repoBaseDirPath + "target/surefire-reports/";
         final String CSV_COVERAGE_REPORT_PATH = repoBaseDirPath + "target/site/jacoco/jacoco.csv";
         final String CLASS_NAME_KEY = "CLASS";
         final String METHODS_MISSED_KEY = "METHOD_MISSED";
         final String METHODS_COVERED_KEY = "METHOD_COVERED";
+        System.out.println();
         printPWD(repoBaseDirPath);
+        System.out.println();
+        StringBuilder finalReportSB = new StringBuilder();
 
         // Execute tests
 
@@ -57,7 +60,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         boolean foundPOM = false;
         if (baseDirFiles == null) {
             System.out.println("The path of " + repoBaseDirPath + " is wrong or the repo is empty! Aborting testing.");
-            return;
+            return "The path of " + repoBaseDirPath + " is wrong or the repo is empty! No tests were ran.";
         }
         else {
             for (File f : baseDirFiles) {
@@ -69,7 +72,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         }
         if (!foundPOM) {
             System.out.println("Repo at " + repoBaseDirPath + " contains no pom.xml! Aborting testing.");
-            return;
+            return "Repo at " + repoBaseDirPath + " contains no pom.xml! No tests were ran.";
         }
         ProcessBuilder pb = new ProcessBuilder();
         pb.directory(new File(repoBaseDirPath));
@@ -78,24 +81,23 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         try {
             Process process = pb.start();
             int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                System.out.println("Success for ProcessBuilder process!");
-            } else {
-                System.out.println("Exited ProcessBuilder process abnormally. Exit val: " + exitVal);
-            }
-        } catch (IOException e) {
-            System.out.println("IO exception when trying to execute ProcessBuilder process");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted exception when trying to execute ProcessBuilder process");
-            e.printStackTrace();
+            System.out.println("Exited ProcessBuilder process 'mvn test' with value: " + exitVal);
+        }
+        catch (IOException e) {
+            System.out.println("IO exception when trying to execute ProcessBuilder 'mvn test' process");
+            return "IO exception when trying to execute ProcessBuilder 'mvn test' process";
+        }
+        catch (InterruptedException e) {
+            System.out.println("InterruptedException when trying to execute ProcessBuilder 'mvn test' process");
+            return "InterruptedException when trying to execute ProcessBuilder 'mvn test' process";
         }
         ArrayList<String> testResults = new ArrayList<>();
         try {
             File dir = new File(TEST_REPORT_FOLDER);
             File[] files = dir.listFiles();
             if (files == null) {
-                System.out.println("No test reports found!");
+                System.out.println("No test reports found! Expected path for reports: " + TEST_REPORT_FOLDER);
+                return "No test reports found! Expected path for reports: " + TEST_REPORT_FOLDER;
             }
             else {
                 for (File f:files) {
@@ -111,19 +113,22 @@ public class ContinuousIntegrationServer extends AbstractHandler {
                     testResults.add(sb.toString());
                 }
             }
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             System.out.println("Error when trying to find test report files. Supplied path:\n" +
                 TEST_REPORT_FOLDER);
-            e.printStackTrace();
+            return "Error when trying to find test report files. Supplied path:\n" + TEST_REPORT_FOLDER;
         }
         if (!testResults.isEmpty()) {
-            System.out.println("-------------------------------------Test results-------------------------------------");
+           finalReportSB.append("----------------------------------Test results---------------------------------\n");
             for (String s: testResults) {
-                System.out.println(s);
+                finalReportSB.append(s);
+                finalReportSB.append("\n");
             }
         }
         else {
-            System.out.println("-------------------------------------No test results to present-------------------------------------");
+            System.out.println("No test results to present. Perhaps the test report folder had no .txt files.");
+            return "No test results to present. Perhaps the test report folder had no .txt files.";
         }
 
         // Branch coverage
@@ -138,23 +143,29 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         catch (FileNotFoundException e) {
             System.out.println("Could not find file for method coverage. Supplied path:\n" +
                 CSV_COVERAGE_REPORT_PATH);
+            finalReportSB.append("Could not find file for method coverage. Supplied path:\n");
+            finalReportSB.append(CSV_COVERAGE_REPORT_PATH);
+            return finalReportSB.toString();
         }
         catch (IOException e) {
             System.out.println("IOException when trying to read file for branch coverage. Supplied path:\n" +
                 CSV_COVERAGE_REPORT_PATH);
+            finalReportSB.append("IOException when trying to read file for branch coverage. Supplied path:\n");
+            finalReportSB.append(CSV_COVERAGE_REPORT_PATH);
+            return finalReportSB.toString();
         }
         if (records.size() != 2) {
-            throw new IOException("Unexpected input from method coverage report. Expected a CSV file with " +
+            throw new IllegalArgumentException("Unexpected input from method coverage report. Expected a CSV file with " +
                 "one line of identifiers and one line of values. Received " + records.size() + " lines.");
         }
         HashMap<String, String> reportMap = new HashMap<>();
         for (int i = 0; i < records.get(0).size(); i++) {
             reportMap.put(records.get(0).get(i), records.get(1).get(i));
         }
-        System.out.println("In the class " + reportMap.get(CLASS_NAME_KEY) + " the number of methods covered " +
-            "by tests is: " + reportMap.get(METHODS_COVERED_KEY) + ",\n" +
-            "and the number of methods not covered by tests is: " + reportMap.get(METHODS_MISSED_KEY));
-
+        finalReportSB.append("In the class ").append(reportMap.get(CLASS_NAME_KEY)).append(" the number of methods covered ")
+            .append("by tests is: ").append(reportMap.get(METHODS_COVERED_KEY)).append(",\n")
+            .append("and the number of methods not covered by tests is: ").append(reportMap.get(METHODS_MISSED_KEY));
+        return finalReportSB.toString();
     }
 
     private void printPWD(String path) {
@@ -174,7 +185,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
                 output.append("\n");
             }
             System.out.println("PWD of the supplied path is:");
-            System.out.println(output);
+            System.out.print(output);
         }
         catch (Exception e) {
             System.out.println("Error in printing PWD. Perhaps the path was non-existent.");
@@ -195,7 +206,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         server.start();
         server.join();*/
         ContinuousIntegrationServer ciServer = new ContinuousIntegrationServer();
-        ciServer.executeTests("./");
+        System.out.println(ciServer.executeTests("./"));
     }
 }
 
