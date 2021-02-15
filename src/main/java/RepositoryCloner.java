525bb4e2f2f164da59aed270c021f8a70319b1b2
Eleonora B <eleonoraborzi@gmail.com>
2021-02-15 09:37:09 +0100

    Issue #40 Doc Adds Documentation for RepoUtils and RepoCLoner

--- src/main/java/RepositoryCloner.java
+++ src/main/java/RepositoryCloner.java
@@ -9,6 +9,15 @@ public class RepositoryCloner {
     
     public static final String tempDirectoryPath = "clonedRepo";
 
+    /**
+     * Function clones a repository from git into a local folder called "clonedRepo"
+     * @param repoHttpsAdress the cloning address from github
+     * @param branchName
+     * @param repoName
+     * @return
+     * @throws IOException 
+     * @throws GitAPIException if it does not find the repo
+     */
     static String cloneRepo(String repoHttpsAdress , String branchName, String repoName) throws IOException, GitAPIException {
 
         try {
@@ -30,6 +39,11 @@ public class RepositoryCloner {
 
     }
 
+    /**
+     * Function that deletes a directory that contains the cloned repo.
+     * If the directory does not exist it return an Illegal Argument Exception.
+     * @param repo File that contains the directory to the cloned repo
+     */
     public static void deleteRepo(File repo){
         if(!repo.isDirectory()){
             throw new IllegalArgumentException("Repo does not exist on this directory ");
